ccf58a1dbc8407d8a850f9952b4c5ae24773147f
Neeraj <chimpoo11496@gmail.com>
2026-08-18 10:03:15 +0200

    Update all

--- Projects/11_I2C_Communication/i2c_master.vhd
+++ Projects/11_I2C_Communication/i2c_master.vhd
@@ -23,56 +23,72 @@ end i2c_master;
 
 architecture behavioral of i2c_master is
 
+    ----------------------------------------------------------------
+    -- I2C MASTER FSM STATES
+    ----------------------------------------------------------------
     type state_type is (
         IDLE,
-        START,
+        START_COND,
         SEND_ADDRESS,
         ADDRESS_ACK,
         SEND_DATA,
         DATA_ACK,
-        STOP
+        STOP_COND
     );
 
     signal state : state_type := IDLE;
 
+
+    ----------------------------------------------------------------
+    -- DATA REGISTERS
+    ----------------------------------------------------------------
     signal address_reg : std_logic_vector(7 downto 0);
     signal data_reg    : std_logic_vector(7 downto 0);
 
     signal bit_count : integer range 0 to 7 := 7;
 
+
+    ----------------------------------------------------------------
+    -- I2C SIGNAL CONTROL
+    ----------------------------------------------------------------
     signal scl_reg : std_logic := '1';
 
-    -- 1 = pull SDA LOW
-    -- 0 = release SDA
+    -- '1' = pull SDA LOW
+    -- '0' = release SDA
     signal sda_low : std_logic := '0';
+
 
 begin
 
     ----------------------------------------------------------------
-    -- Outputs
+    -- SCL OUTPUT
     ----------------------------------------------------------------
-
     scl <= scl_reg;
 
-    -- Open-drain style SDA
+
+    ----------------------------------------------------------------
+    -- SDA OPEN-DRAIN STYLE OUTPUT
+    ----------------------------------------------------------------
     sda <= '0' when sda_low = '1' else 'Z';
 
 
     ----------------------------------------------------------------
-    -- Main FSM
+    -- MAIN I2C FSM
     ----------------------------------------------------------------
-
     process(clk)
     begin
 
         if rising_edge(clk) then
 
+            --------------------------------------------------------
+            -- RESET
+            --------------------------------------------------------
             if reset = '1' then
 
-                state     <= IDLE;
+                state <= IDLE;
 
-                scl_reg   <= '1';
-                sda_low   <= '0';
+                scl_reg <= '1';
+                sda_low <= '0';
 
                 busy      <= '0';
                 done      <= '0';
@@ -83,12 +99,18 @@ begin
 
                 bit_count <= 7;
 
+
+            --------------------------------------------------------
+            -- NORMAL OPERATION
+            --------------------------------------------------------
             else
 
                 -- DONE is normally LOW
                 done <= '0';
 
+
                 case state is
+
 
                     ------------------------------------------------
                     -- IDLE
@@ -102,21 +124,24 @@ begin
 
                         if start = '1' then
 
+                            -- Combine 7-bit address + WRITE bit
                             address_reg <= slave_addr & '0';
-                            data_reg    <= data_in;
+
+                            data_reg <= data_in;
 
                             bit_count <= 7;
 
-                            busy  <= '1';
-                            state <= START;
+                            busy <= '1';
+
+                            state <= START_COND;
 
                         end if;
 
 
                     ------------------------------------------------
-                    -- START
+                    -- START CONDITION
                     ------------------------------------------------
-                    when START =>
+                    when START_COND =>
 
                         -- SCL HIGH
                         -- SDA HIGH -> LOW
@@ -132,16 +157,24 @@ begin
                     ------------------------------------------------
                     when SEND_ADDRESS =>
 
-                        -- Put address bit on SDA
+                        -- SCL LOW
+
                         scl_reg <= '0';
 
+
+                        -- Send current address bit
+
                         if address_reg(bit_count) = '0' then
+
                             sda_low <= '1';
+
                         else
+
                             sda_low <= '0';
+
                         end if;
 
-                        -- Next state
+
                         state <= ADDRESS_ACK;
 
 
@@ -151,21 +184,28 @@ begin
                     when ADDRESS_ACK =>
 
                         -- Release SDA
+
                         sda_low <= '0';
 
-                        -- Clock HIGH
+
+                        -- SCL HIGH
+
                         scl_reg <= '1';
 
+
                         -- For this simple simulation,
-                        -- assume slave ACKs.
+                        -- assume slave ACK is received.
+
                         if bit_count = 0 then
 
                             bit_count <= 7;
+
                             state <= SEND_DATA;
 
                         else
 
                             bit_count <= bit_count - 1;
+
                             state <= SEND_ADDRESS;
 
                         end if;
@@ -176,13 +216,23 @@ begin
                     ------------------------------------------------
                     when SEND_DATA =>
 
+                        -- SCL LOW
+
                         scl_reg <= '0';
 
+
+                        -- Send current data bit
+
                         if data_reg(bit_count) = '0' then
+
                             sda_low <= '1';
+
                         else
+
                             sda_low <= '0';
+
                         end if;
+
 
                         state <= DATA_ACK;
 
@@ -192,26 +242,35 @@ begin
                     ------------------------------------------------
                     when DATA_ACK =>
 
+                        -- Release SDA
+
                         sda_low <= '0';
+
+
+                        -- SCL HIGH
 
                         scl_reg <= '1';
 
+
+                        -- Assume ACK
+
                         if bit_count = 0 then
 
-                            state <= STOP;
+                            state <= STOP_COND;
 
                         else
 
                             bit_count <= bit_count - 1;
+
                             state <= SEND_DATA;
 
                         end if;
 
 
                     ------------------------------------------------
-                    -- STOP
+                    -- STOP CONDITION
                     ------------------------------------------------
-                    when STOP =>
+                    when STOP_COND =>
 
                         -- SCL HIGH
                         -- SDA LOW -> HIGH
@@ -220,9 +279,11 @@ begin
                         sda_low <= '0';
 
                         busy <= '0';
+
                         done <= '1';
 
                         state <= IDLE;
+
 
                 end case;
 
